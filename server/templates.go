d24734b91062352ad261b4aea3bb79783d50215c
mgeri <m.geri1974@gmail.com>
2019-08-25 16:31:15 +0200

    Unit test samples for handlers and middleware

--- server/templates.go
+++ server/templates.go
@@ -26,7 +26,13 @@ type templateData struct {
 // Create a humanDate function which returns a nicely formatted string
 // representation of a time.Time object.
 func humanDate(t time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
+	// Return the empty string if time has the zero value.
+	if t.IsZero() {
+		return ""
+	}
+
+	// Convert the time to UTC before formatting it.
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 // Initialize a template.FuncMap object and store it in a global variable. This is
---
+++ server/templates_test.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_humanDate(t *testing.T) {
+	// Create a slice of anonymous structs containing the test case name,
+	// input to our humanDate() function (the tm field), and expected output
+	// (the want field).
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.UTC),
+			want: "17 Dec 2020 at 10:00",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "17 Dec 2020 at 09:00",
+		},
+	}
+
+	// Loop over the test cases.
+	for _, tt := range tests {
+		// Use the t.Run() function to run a sub-test for each test case. The
+		// first parameter to this is the name of the test (which is used to
+		// identify the sub-test in any log output) and the second parameter is
+		// and anonymous function containing the actual test for each case.
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+
+			if hd != tt.want {
+				t.Errorf("want %q; got %q", tt.want, hd)
+			}
+		})
+	}
+}
